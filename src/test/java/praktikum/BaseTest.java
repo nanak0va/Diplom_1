cd8f7754c4afcdd554bc3c5712ba2d9ad3a35393
Anastasiia Kraeva <nanak0va@ya.ru>
2025-12-22 11:14:25 +0300

    feat: - Добавила Unit тесты для методов класса Burger. - Добавила описание задания readme.md

---
+++ src/test/java/praktikum/BaseTest.java
@@ -0,0 +1,24 @@
+package praktikum;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class BaseTest {
+
+    public static final float DELTA = 0.001f;
+
+    protected static Bun mockBun(String name, float price) {
+        Bun bun = mock(Bun.class);
+        when(bun.getName()).thenReturn(name);
+        when(bun.getPrice()).thenReturn(price);
+        return bun;
+    }
+
+    protected static Ingredient mockIngredient(IngredientType type, String name, float price) {
+        Ingredient ingredient = mock(Ingredient.class);
+        when(ingredient.getType()).thenReturn(type);
+        when(ingredient.getName()).thenReturn(name);
+        when(ingredient.getPrice()).thenReturn(price);
+        return ingredient;
+    }
+}
